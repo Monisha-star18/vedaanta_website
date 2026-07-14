fa1f88fb2abadd11f4e8668056f3af9867cd149d
Monisha-star18 <monisha.r180505@gmail.com>
2026-07-14 17:39:57 +0530

    Features section done

--- scripts/landing.js
+++ scripts/landing.js
@@ -34,5 +34,39 @@ $(document).ready(function(){
 
     })
 
+
+    $(".features-carosel").slick({
+        slidesToShow: 2,
+        slidesToScroll :1,
+        infinite :true,
+        autoplay :true,
+        autoplaySpeed: 2000,
+        speed: 800,
+        arrows: true,
+        dots: false,
+
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+
+    })
+
     
 })
