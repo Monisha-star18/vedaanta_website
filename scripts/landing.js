bb97df3dbe35eeec5682c40c055ca8451a05c78b
Monisha-star18 <monisha.r180505@gmail.com>
2026-07-14 16:06:18 +0530

    added the carosel

---
+++ scripts/landing.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+
+    $(".home-carosel-container").slick({
+        slidesToShow: 3,
+        slidesToScroll :1,
+        infinite :true,
+        autoplay :true,
+        autoplaySpeed: 2000,
+        speed: 800,
+        arrows: true,
+        dots: false,
+
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+
+    })
+
+    
+})
